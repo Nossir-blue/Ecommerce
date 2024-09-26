f9048eddde8a8da3505332c029eeda8fb4f812bc
Roger Reis <rogerreis20@protonmail.com>
2024-09-26 01:53:38 +0100

    need to fix bug in updateTotal. Something is not working properly with the floating point numbers
    need to fix bug in addProductToCart because i
    can't add product to the cart

--- js/main.js
+++ js/main.js
@@ -34,12 +34,19 @@ function ready(){
         let input = quantityInputs[i];
         input.addEventListener('change', quantityChanged)
     }
+    // adicionar ao carrinho
+    let addCart = document.getElementsByClassName('add-cart');
+    for(let i = 0; i < addCart.length; i++){
+        let button = addCart[i];
+        button.addEventListener('click', addCartClicked)
+    }
 }
 
 // remover item do carrinho
 function removeCartItem(event){
-    let butotnClicked = event.target;
-    butotnClicked.parentElement.remove();
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    updateTotal();
 }
 
 // quantityChanged
@@ -52,10 +59,50 @@ function quantityChanged(event){
     updateTotal();
 }
 
+// função adicionar carro
+function addCartClicked(event){
+    let button = event.target;
+    let shopProducts = button.parentEelement;
+    let title = shopProducts.getElementsByClassName("product-title").innerText;
+    let price = shopProducts.getElementsByClassName("price"),innerText;
+    let productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(title, price, productImg);
+    updateTotal();
+}
+
+function addProductToCart(title, price, productImg){
+    let cartShopBox = document.createElement('div');
+    cartShopBox.classList.add('cart-box');
+    let cartItems = document.getElementsByClassName('cart-content')[0];
+    let cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
+    for(let i = 0; i < cartItemsNames.length; i++){
+        if(cartItemsNames[i].innerText == title){
+            alert('Tu já tens este ítem adicionado no carrinho');
+            return;
+        }
+    }
+    let cartBoxContent = `<img src="${productImg}" alt="cart-box" class="cart-img">
+                        <div class="detail-box">
+                            <div class="cart-product-title">
+                                ${title}
+                            </div>
+                            <div class="cart-price">
+                                ${price}KZ
+                            </div>
+                            <input type="number" name="" id="" value="1" class="cart-quantity">
+                        </div>
+                        <!-- remover item -->
+                        <box-icon class="bx bx-trash-alt cart-remove" name='x'></box-icon>`;
+    cartShopBox.innerHTML = cartBoxContent;
+    cartItems.append(cartShopBox);
+    cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem);
+    cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged);
+}
+
 // atualizar total
 function updateTotal(){
     let cartContent = document.getElementsByClassName('cart-content')[0];
-    let cartBoxes = cartContent.getElementsByClassName('cart-box')[0];
+    let cartBoxes = cartContent.getElementsByClassName('cart-box');
     let total = 0;
     for(let i = 0; i < cartBoxes.length; i++){
         let cartBox = cartBoxes[i];
@@ -64,7 +111,7 @@ function updateTotal(){
         let price = parseFloat(priceElement.innerText.replace('KZ',''));
         let quantity = quantityElement.value;
         total += price * quantity;
-
-        document.getElementsByClassName('total-price')[0].innerText = total + "KZ";
     }
+    total = Math.round(total * 100) / 100;
+    document.getElementsByClassName('total-price')[0].innerText = total + "KZ";
 }
